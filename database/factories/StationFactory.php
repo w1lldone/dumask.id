8453cbf12b6934d9c0ad8b84aa230338df686d22
w1lldone <wildan.arrahman@gmail.com>
2021-03-06 18:51:55 +0700

    Station model and factory

---
+++ database/factories/StationFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Station;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Station::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'address' => $this->faker->address,
+            'description' => $this->faker->sentence(),
+            'longitude' => $this->faker->longitude,
+            'latitude' => $this->faker->latitude,
+        ];
+    }
+}
