1cba408439b27b52d51dbef743935ade1617de8b
w1lldone <wildan.arrahman@gmail.com>
2021-03-07 13:05:55 +0700

    Add with distance scope

--- tests/Feature/Models/StationModelTest.php
+++ tests/Feature/Models/StationModelTest.php
@@ -18,4 +18,30 @@ class StationModelTest extends TestCase
 
         $this->assertDatabaseCount('stations', 5);
     }
+
+    public function it_can_select_distances()
+    {
+        $farthest = Station::factory()->create([
+            'name' => 'Plaza Ambarukmo',
+            'latitude' => -7.782721,
+            'longitude' => 110.401181,
+        ]);
+
+        $middle = Station::factory()->create([
+            'name' => 'kantor pusat',
+            'latitude' => -7.767970,
+            'longitude' => 110.378565,
+        ]);
+
+        $nearest = Station::factory()->create([
+            'name' => 'RS Sardjito',
+            'latitude' => -7.768735,
+            'longitude' => 110.373372,
+        ]);
+
+        $latitude = -7.777217;
+        $longitude = 110.331988;
+        $stations = Station::withDistance($longitude, $latitude)->orderBy('distance', 'asc')->get();
+        $this->assertEquals($stations->first()->id, $nearest->id);
+    }
 }
