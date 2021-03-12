3d69660aafa1ae5a007bd24aebc91cd4728f7d05
Michael Roberto <michaelr1300@gmail.com>
2021-03-12 15:21:28 +0700

    Update Explore Page
    - Update explore sesuai desain di Figma

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,10 +4,10 @@
     }
 </style>
 
-<div class="flex align-middle sm:items-center w-100" style="background: #C4C4C4">
+<div class="d-flex align-middle sm:items-center w-100" style="background: #C4C4C4">
     <p class="text-center text-white mx-auto my-auto py-2">
         DUMASK.ID Brought to you by 
-        <a class="footer-link" href="https://piat.ugm.ac.id/" target="_blank"><u>Pusat Inovasi Agroteknologi UGM</u></a>  
+        <a class="footer-link text-white" href="https://piat.ugm.ac.id/" target="_blank"><u>Pusat Inovasi Agroteknologi UGM</u></a>  
         developed by Smart System Research Group UGM
     </p>
 </div>
