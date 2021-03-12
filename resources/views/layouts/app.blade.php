9a8866c71d7047a8a3434b42cf3fd5d4799ebb57
Wildan Ainurrahman <w1lldone@users.noreply.github.com>
2021-03-12 17:49:27 +0700

    Feature/stations endpoint (#15)
    * WIP
    
    * Load stations on map

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,7 @@
       
       <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+      <script src="{{ asset('/js/app.js') }}"></script>
       <script>
         new Vue({
           el: '#app',
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\StationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('stations')->name('stations.')->group(function ()
+{
+    Route::get('/', [StationController::class, 'index'])->name('index');
 });
