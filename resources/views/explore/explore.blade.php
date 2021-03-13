57c6898de6a99f855d7cca533f17f4d83e7ba9cd
Michael Roberto <46708593+michaelr1300@users.noreply.github.com>
2021-03-13 22:53:16 +0700

    Handle Location (#20)
    * Handle Location
    
    - Load map + station location
    - Load user location on button click
    - Dynamic Marker
    
    * Update explore.blade.php
    
    - Rename function
    - Fitbound

--- resources/views/explore/explore.blade.php
+++ resources/views/explore/explore.blade.php
@@ -2,37 +2,46 @@
 @section('content')
 
 <div class="container w-md-75 my-4">
-    <h2 class="text-primary text-center font-weight-bold mt-2 mb-0">Yuk, Buang Maskermu Pada Tempatnya !</h2>
-    <p class="text-secondary text-center font-weight-bold">Temukan dropboxes DUMASK.ID di sekitar mu</p>
+    <h2 class="text-primary text-center font-weight-bold mt-2 mb-0">YUK, BUANG MASKER KITA PADA TEMPATNYA</h2>
+    <p class="text-secondary text-center font-weight-bold">Temukan dropboxes DUMASK.ID di sekitarmu</p>
 
-    <form action="">
-        <div class="d-md-flex justify-content-between form-group mx-auto">
-            <div class="col-md-8 col-12">
+    <div class="d-md-flex justify-content-between form-group mx-auto">
+        <div class="col-md-8 col-12">
+            <form action="">
                 <input type="text" placeholder="Cari Station" class="form-control shadow">
-            </div>
-            <div class="d-flex col-md-4 col-12 justify-content-between">
-                <button class="btn btn-primary shadow mr-2"><img src="{{ asset('img/icon_location.svg')}}"></i></button>
-                <button class="btn btn-primary shadow ml-2 w-100">Station Terdekat</i></button>
-            </div>
-            
+            </form>
         </div>
-    </form>
+        <div class="d-flex col-md-4 col-12 justify-content-between">
+            <button id="btn-location" onclick="getLocation()" class="btn btn-primary shadow mr-2"><img src="{{ asset('img/icon_location.svg')}}" class="mx-auto my-auto"></i></button>
+            <button class="btn btn-primary shadow ml-2 w-100">Station Terdekat</i></button>
+        </div>
+    </div>
+
     <div id="map-id" class="mx-auto" style="height: 400px;"></div>
 </div>
 
 @include('layouts.footer')
     
-<script>            
-    navigator.geolocation.getCurrentPosition(async function(location) {
-    
-    var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+<script>
+$( document ).ready(initialize);
+
+//Global Map
+var gMap;
+
+//User Marker
+var userMarker;
+var mapHasUserMarker = false;
+
+var markerArray = [];
+
+async function initialize()
+{
+    var latlng = new L.LatLng(-7.770717, 110.377724);
 
     var mymap = L.map('map-id').setView(latlng, 13)
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
-
-    var marker = L.marker(latlng).addTo(mymap);
 
     response = await axios.get('/api/stations')
 
@@ -45,11 +54,55 @@
 
         markers.push(latlng)
 
-        mymap.fitBounds(markers, {
+        markerArray = markers;
+
+        mymap.fitBounds(markerArray, {
             padding: [20, 20]
         })
     }
 
-    });
+    gMap = mymap;
+}
+
+function getLocation()
+{ 
+    navigator.geolocation.getCurrentPosition(
+        async function(location) {
+            var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+            if(mapHasUserMarker == false)
+            {
+                userMarker = L.marker(latlng);
+                gMap.addLayer(userMarker);
+                markerArray.push(latlng);
+                mapHasUserMarker = true;
+
+                gMap.fitBounds(markerArray, {
+                    padding: [20, 20]
+                })
+            }
+            else
+            {
+                // Remove Marker
+                gMap.removeLayer(userMarker);
+
+                // Add new marker
+                userMarker = L.marker(latlng);
+                gMap.addLayer(userMarker);
+
+                markerArray.pop();
+                markerArray.push(latlng);
+
+                gMap.fitBounds(markerArray, {
+                    padding: [20, 20]
+                })
+            }
+        },
+        async function(err)
+        {
+            alert("Silakan nyalakan lokasi dan coba lagi.")
+        }
+        );
+}
+
 </script>
 @endsection
