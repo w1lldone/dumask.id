181501cc466523e61827462819001a7e24fb5e84
w1lldone <wildan.arrahman@gmail.com>
2021-03-16 16:24:24 +0700

    assets versioning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <!-- Font Awesome -->
         {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js" integrity="sha512-UwcC/iaz5ziHX7V6LjSKaXgCuRRqbTp1QHpbOJ4l1nw2/boCfZ2KlFIqBUA/uRVF0onbREnY9do8rM/uT/ilqw==" crossorigin="anonymous"></script> --}}
 
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 
     </head>
 
@@ -35,7 +35,7 @@
               
               @yield('content')
         </div>
-      <script src="{{ asset('/js/app.js') }}"></script>
+      <script src="{{ mix('/js/app.js') }}"></script>
   </body>
   @include('layouts.footer')
 </html>
