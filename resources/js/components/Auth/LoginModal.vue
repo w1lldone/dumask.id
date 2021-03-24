509545f8dc9e624dc763762c80d4938b405ae427
Michael Roberto <46708593+michaelr1300@users.noreply.github.com>
2021-03-24 21:37:22 +0700

    Login Register Modal (#34)
    * Login Register Modal
    
    * Resolve Conflict + Compile
    
    * Update app.blade.php

---
+++ resources/js/components/Auth/LoginModal.vue
@@ -0,0 +1,273 @@
+<template>
+  <div>
+    <li class="nav-item">
+        <a 
+        class="nav-link text-primary font-weight-bold"
+        data-toggle="modal"
+        data-target="#login-modal"
+        href="#"
+        >
+        MASUK
+        </a>
+    </li>
+    <div
+      class="modal fade"
+      id="login-modal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="modal-id"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" v-show="isPageLogin" role="document">
+        <div class="modal-content">
+          <div class="modal-header border-0 mx-4">
+              <div class="flex">
+                <h2
+                class="modal-title font-weight-bold text-primary"
+                >
+                MARI BERGABUNG
+                </h2>
+                <h5
+                class="modal-title font-weight-bold text-secondary"
+                >
+                BERSAMA DUMASK.ID
+                </h5>
+              </div>
+            
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body mx-4">
+            <button class="btn btn-secondary w-100 my-2">
+                <div class="d-flex">
+                    <span class="ml-auto pr-3 mdi mdi-google"></span>
+                    <span class="mr-auto">MASUK DENGAN GOOGLE</span>
+                </div>
+            </button>   
+            
+            <p 
+                class="text-center text-muted w-100 my-4" 
+                style="border-bottom: 1px solid #D5D5D5; line-height: 0.1em; margin: 10px 0 20px;"
+            >
+                <span style="background:#fff; padding:0 10px;">atau</span>
+            </p>
+
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email"
+              required 
+              v-model="form.email" 
+              class="form-control"
+              :class="{ 'is-invalid': hasErrors('email') }"
+              />
+              <div class="invalid-feedback">
+                {{ getErrors("email") }}
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" v-model="form.password" class="form-control" :class="{ 'is-invalid': hasErrors('password') }"/>
+              <div class="invalid-feedback">
+                {{ getErrors("password") }}
+              </div>
+            </div>
+            <div class="form-group mt-4 text-right">
+              <button
+                class="btn btn-primary w-100"
+                v-show="!isLoading"
+                @click="doLogin()"
+              >
+                MASUK
+              </button>
+              <button class="btn btn-primary w-100" disabled v-show="isLoading">
+                MASUK
+              </button>
+            </div>
+            <p class="text-center text-muted mt-2">
+                Belum punya akun? Daftar
+                <span><a href="#" @click="toggleModalType()">di sini.</a></span>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="modal-dialog modal-dialog-centered" v-show="!isPageLogin" role="document">
+        <div class="modal-content">
+          <div class="modal-header border-0 mx-4">
+              <div class="flex">
+                <h2
+                class="modal-title font-weight-bold text-primary"
+                >
+                MARI BERGABUNG
+                </h2>
+                <h5
+                class="modal-title font-weight-bold text-secondary"
+                >
+                BERSAMA DUMASK.ID
+                </h5>
+              </div>
+            
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body mx-4">
+            <button class="btn btn-secondary w-100 my-2">
+                <div class="d-flex">
+                    <span class="ml-auto pr-3 mdi mdi-google"></span>
+                    <span class="mr-auto">DAFTAR DENGAN GOOGLE</span>
+                </div>
+            </button>   
+            
+            <p 
+                class="text-center text-muted w-100 my-4" 
+                style="border-bottom: 1px solid #D5D5D5; line-height: 0.1em; margin: 10px 0 20px;"
+            >
+                <span style="background:#fff; padding:0 10px;">atau</span>
+            </p>
+
+            <div class="form-group">
+              <label for="name">Nama</label>
+              <input type="name"
+              required 
+              v-model="form.name" 
+              class="form-control"
+              :class="{ 'is-invalid': hasErrors('name') }"
+              />
+              <div class="invalid-feedback">
+                {{ getErrors("name") }}
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email"
+              required 
+              v-model="form.email" 
+              class="form-control"
+              :class="{ 'is-invalid': hasErrors('email') }"
+              />
+              <div class="invalid-feedback">
+                {{ getErrors("email") }}
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" v-model="form.password" class="form-control" :class="{ 'is-invalid': hasErrors('password') }"/>
+              <div class="invalid-feedback">
+                {{ getErrors("password") }}
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="password_confirmation">Confirm Password</label>
+              <input type="password" v-model="form.password_confirmation" class="form-control" :class="{ 'is-invalid': hasErrors('password_confirmation') }"/>
+              <div class="invalid-feedback">
+                {{ getErrors("password_confirmation") }}
+              </div>
+            </div>
+            <div class="form-group mt-4 text-right">
+              <button
+                class="btn btn-primary w-100"
+                v-show="!isLoading"
+                @click="doRegister()"
+              >
+                DAFTAR
+              </button>
+              <button class="btn btn-primary w-100" disabled v-show="isLoading">
+                DAFTAR
+              </button>
+            </div>
+            <p class="text-center text-muted mt-2">
+                Sudah punya akun? Masuk
+                <span><a href="#" @click="toggleModalType()">di sini.</a></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginModal",
+
+  data() {
+    return {
+      form: {
+        name: null,
+        email: null,
+        password: null,
+        password_confirmation: null,
+      },
+      isLoading: false,
+      isPageLogin: true,
+      errors: {},
+    };
+  },
+
+  methods: {
+    async doLogin() {
+      this.isLoading = true;
+
+      try {
+        let response = await axios.post("/login", this.form);
+        return location.reload();
+      } catch (error) {
+        console.log(error.response);
+        this.errors = error.response.data.errors;
+      }
+
+      this.isLoading = false;
+    },
+
+    async doRegister() {
+      this.isLoading = true;
+
+      try {
+        let response = await axios.post("/register", this.form);
+        return location.reload();
+      } catch (error) {
+        console.log(error.response);
+        this.errors = error.response.data.errors;
+      }
+
+      this.isLoading = false;
+    },
+
+    toggleModalType(){
+      this.isPageLogin = !this.isPageLogin;
+    },
+
+    
+
+    hasErrors(key) {
+      if (this.errors[key]) {
+        return true;
+      }
+
+      return false;
+    },
+    getErrors(key) {
+      if (this.hasErrors(key)) {
+        return this.errors[key].join(", ");
+      }
+
+      return "";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,16 +23,89 @@
     
     <body class="relative antialiased">
         <div id="app" class="items-top d-flex flex-column justify-content-between min-h-screen sm:items-center sm:pt-0 sm:p-3">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow rounded relative w-100">
+            <nav class="navbar navbar-expand-md navbar-light bg-white rounded relative w-100">
                 <div class="container py-0">
                     <a class="navbar-brand" href="{{ url('/') }}">
                         <img class="img-fluid" src="{{ asset('img/logo_navbar.svg')}}" alt="Dumask.id">
                     </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a 
+                                class="nav-link font-weight-bold px-3 {{ Request::is('/') ? 'text-primary' : 'text-secondary' }}" 
+                                href=""
+                                >
+                                Home
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a 
+                                class="nav-link text-secondary font-weight-bold px-3 {{ Request::is('/about') ? 'text-primary' : 'text-secondary' }}" 
+                                href=""
+                                >
+                                About Us
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a 
+                                class="nav-link text-secondary font-weight-bold px-3 {{ Request::is('/contact') ? 'text-primary' : 'text-secondary' }}" 
+                                href=""
+                                >
+                                Contact
+                                </a>
+                            </li>
+                        </ul>
+    
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <login-modal></login-modal>
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link text-primary font-weight-bold dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
+    
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item disabled" href="">Profil</a>
+
+                                        <a class="dropdown-item text-primary" href="{{ route('dashboard') }}" >Dashboard</a>
+                                        
+                                        @can('viewAny', \App\Models\User::class)
+                                            <a class="dropdown-item text-primary" href="{{ route('user.index') }}" >Manage User</a>
+                                            {{-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                                <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
+                                                    {{ __('Users') }}
+                                                </x-nav-link>
+                                            </div> --}}
+                                        @endcan
+                                        
+                                        {{-- Logout --}}
+                                        <a class="dropdown-item text-primary" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                                           {{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+
+                                        
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
             </nav>
             
-              
-              @yield('content')
+            @yield('content')
         </div>
       <script src="{{ mix('/js/app.js') }}"></script>
   </body>
