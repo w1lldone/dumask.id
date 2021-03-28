f40e98050cc89fbb1bd461b6af73e1964f12af4c
Wildan Ainurrahman <w1lldone@users.noreply.github.com>
2021-03-28 09:54:47 +0700

    Add Station dropbox actions (#46)

---
+++ app/Http/Controllers/StationDropboxController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dropbox;
+use App\Models\Station;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class StationDropboxController extends Controller
+{
+    public function store(Station $station, Request $request)
+    {
+        $this->authorize('update', $station);
+
+        $data = $request->validate([
+            'model' => ['required', 'string', Rule::in(Dropbox::$availableModels)],
+            'color' => ['required', 'string', Rule::in(Dropbox::$availableColors)]
+        ]);
+
+        $dropbox = $station->dropboxes()->create($data);
+
+        return $dropbox;
+    }
+
+    public function update(Request $request, Station $station, $dropbox)
+    {
+        $this->authorize('update', $station);
+        $dropbox = $station->dropboxes()->findOrFail($dropbox);
+        
+        $data = $request->validate([
+            'model' => ['string', Rule::in(Dropbox::$availableModels)],
+            'color' => ['string', Rule::in(Dropbox::$availableColors)]
+        ]);
+
+        $dropbox->update($data);
+
+        return $dropbox->fresh();
+    }
+
+    public function destroy(Station $station, $dropbox)
+    {
+        $this->authorize('update', $station);
+
+        $dropbox = $station->dropboxes()->findOrFail($dropbox);
+
+        $dropbox->delete();
+
+        return response()->noContent();
+    }
+}
