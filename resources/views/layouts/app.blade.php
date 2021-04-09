993e0f8ce7bd80be0da13a39a92404066f97c3f1
w1lldone <wildan.arrahman@gmail.com>
2021-04-09 07:52:56 +0700

    Fix Analytics tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,15 +8,15 @@
         @env('production')
             <meta name="google-site-verification" content="ZQmYpcWC-8MF6KLnmUF_rYWNLh10NSrhICri74g17gU">
 
-            <!-- Google Tag Manager -->
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-74RFXHTS34"></script>
             <script>
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WWW7BQB');
+                window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-74RFXHTS34');
             </script>
-            <!-- End Google Tag Manager -->
         @endenv
 
         <!-- Fonts -->
@@ -35,12 +35,6 @@
 
     
     <body class="relative antialiased">
-        @env('production')
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWW7BQB" height="0" width="0"
-                style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-        @endenv
         <div id="app" class="items-top d-flex flex-column justify-content-between min-h-screen sm:items-center sm:pt-0 sm:p-3">
             <nav class="navbar navbar-expand-md navbar-light bg-white rounded relative w-100">
                 <div class="container py-0">
