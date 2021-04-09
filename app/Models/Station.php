8a802ab71ffb7582632beb5051ec70e3988c2799
w1lldone <wildan.arrahman@gmail.com>
2021-04-09 14:57:10 +0700

    Add media library to Station Model

--- app/Models/Station.php
+++ app/Models/Station.php
@@ -6,14 +6,21 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Station extends Model
+class Station extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $guarded = ['id'];
     protected $withCount = ['dropboxes'];
     protected $with = ['schedules'];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('images');
+    }
 
     public function dropboxes()
     {
@@ -35,8 +42,7 @@ class Station extends Model
      */
     public function scopeWithDistance($query, $longitude, $latitude)
     {
-        return $query->addSelect(['distance' => function ($q) use ($latitude, $longitude)
-        {
+        return $query->addSelect(['distance' => function ($q) use ($latitude, $longitude) {
             $q->selectRaw("6371 * ACOS(COS(RADIANS({$latitude}))
                                 * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS({$longitude}))
                                 + SIN(RADIANS({$latitude})) * SIN(RADIANS(latitude))) AS distance");
