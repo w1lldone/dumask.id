8c41a3a29e5fecb6b3004c48562bc06698083ca9
Wildan Ainurrahman <w1lldone@users.noreply.github.com>
2021-04-16 13:39:39 +0700

    Draft: Dropbox log (#77)
    * DropboxLog model, migration, factory, and test
    
    * Fix migration
    
    * Fix migrations
    
    * Fix column name

---
+++ tests/Feature/Models/DropboxLogModelTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\DropboxLog;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DropboxLogModelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_be_created_using_factory()
+    {
+        $dropboxLog = DropboxLog::factory()->create();
+
+        $this->assertDatabaseHas('dropbox_logs', $dropboxLog->toArray());
+    }
+
+    /** @test */
+    public function it_has_many_children()
+    {
+        $log = DropboxLog::factory()->create();
+        DropboxLog::factory(5)->create(['parent_id' => $log->id]);
+
+        $this->assertCount(5, $log->children);
+        $this->assertInstanceOf(DropboxLog::class, $log->children->first());
+    }
+
+    /** @test */
+    public function it_belongs_to_a_parent()
+    {
+        $parent = DropboxLog::factory()->create();
+        $child = DropboxLog::factory()->create(['parent_id' => $parent->id]);
+
+        $this->assertTrue($parent->is($child->parent));
+    }
+}
