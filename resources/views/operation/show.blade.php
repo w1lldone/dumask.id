462ec562fc9f9235bd66698a4698635c1315bc86
Michael Roberto <46708593+michaelr1300@users.noreply.github.com>
2021-05-09 20:24:19 -0700

    Operation Detail (#102)

--- resources/views/operation/show.blade.php
+++ resources/views/operation/show.blade.php
@@ -1,35 +1,64 @@
-<x-default-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <a href="{{ route('operation.index') }}" style="text-decoration: none">{{ __('Stations List') }}</a>
-            <span class="mdi mdi-chevron-right"></span>{{ $station->name }} Dropbox Log
-        </h2>
-    </x-slot>
+@extends('layouts.template')
+@section('content')
+    <div class="row">
+        <div class="col-12 flex px-md-0 pt-sm-0 w-100">
+            <div class="col-12 col-md-4 flex card border-0 mt-4 py-2" style="border-radius:0.75em">
+                <h2 class="text-secondary font-weight-bold">OPERATIONS</h2>
+                <span>
+                    <a href="{{ route('operation.index') }}">Operations List</a> 
+                    <span class="mdi mdi-chevron-right"></span> Detail Operation
+                </span>
+            </div>
+        </div>
+    </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="container">
-                    <div class="row py-4 justify-content-between">
-                        @foreach ($dropboxes as $dropbox)
-                        <div class="col-md-6">
-                            <div class="h3">
-                                Dropbox {{ $dropbox->color }} {{ $dropbox->model }}
-                            </div>
-                            @foreach ($dropbox->dropboxLogs as $key => $log)
-                            Pemasangan ke {{ $key+1 }}
+    <div class="mt-4 d-flex-column">
+        <div class="my-3">
+            <a
+                href = "/operation/"
+                class="btn btn-primary "
+            >
+                <span class="mdi mdi-arrow-left pr-2"></span>
+                KEMBALI
+            </a>
+        </div>
+        <div class="flex card border-0 px-4 py-4" style="border-radius:0.75em">
+            <div class="d-flex">
+                <h4 class="text-dark font-weight-bold my-auto">
+                    {{ $station->name }}
+                </h4>
+            </div>
+            <hr style="border-bottom: 2px solid #c4c4c4;">
+
+            <div class="row py-2 justify-content-between">
+                @foreach ($dropboxes as $dropbox)
+                <div class="col-md-6 my-3">
+                    <div class="p-3" style="border: 1px solid #c4c4c4; border-radius:0.75em">
+                        <h5 class="text-secondary font-weight-bold my-auto">
+                            Dropbox {{ $dropbox->color }} {{ $dropbox->model }}
+                        </h5>
+                        <hr style="border-bottom: 2px solid #c4c4c4;">
+    
+                        @foreach ($dropbox->dropboxLogs as $key => $log)
+                        <div class="p-3 my-3" style="border: 1px solid #c4c4c4; border-radius:0.75em">
+                            <h5 class="text-dark font-weight-bold my-auto">
+                                Pemasangan ke {{ $key+1 }}
+                            </h5>
                             @if ($log->final_weight)
-                            Berat bersih {{ $log->final_weight - $log->weight }}
+                            <div class="my-2">
+                                Berat bersih <span class="text-secondary">{{ $log->final_weight - $log->weight }} gram</span>
+                            </div>
                             @endif
-                            <table class="table">
-                                <thead>
+                            <hr style="border-bottom: 2px solid #c4c4c4;">
+                            <table class="table table-borderless table-responsive d-md-table">
+                                <thead style="border-bottom: 1px solid #c4c4c4;">
                                     <tr>
                                         <th>Tanggal</th>
-                                        <th>Berat</th>
+                                        <th>Berat (gram)</th>
                                         <th>Diukur oleh</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="text-secondary">
                                     <tr>
                                         <td>{{ $log->starts_at->format('j F Y') }}</td>
                                         <td>{{ $log->weight }}</td>
@@ -44,13 +73,12 @@
                                     @endforeach
                                 </tbody>
                             </table>
-                            @endforeach
-
                         </div>
                         @endforeach
                     </div>
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
-</x-default-layout>
+@endsection
