f29bb24546bad06fa6bc04a77b4360a7342f4a05
w1lldone <wildan.arrahman@gmail.com>
2021-05-24 19:46:58 +0700

    Add test

--- tests/Feature/StationReportFeatureTest.php
+++ tests/Feature/StationReportFeatureTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Report;
 use App\Models\Station;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
@@ -45,5 +47,54 @@ class StationReportFeatureTest extends TestCase
         $response = $this->postJson(route('station.report.store', $station), $data);
 
         $response->assertCreated();
+    }
+
+    /** @test */
+    public function user_can_resolve_all_reports()
+    {
+        $this->login();
+        $station = Station::factory()->create();
+        /** @var Collection */
+        $reports = Report::factory(5)->create([
+            'station_id' => $station->id
+        ]);
+
+        $response = $this->putJson(
+            route('station.report.resolve', $station),
+            [
+                'resolve_all' => true,
+            ]
+        );
+
+        $response->assertOk();
+        $this->assertDatabaseMissing('reports', [
+            'resolver_id' => null,
+            'resolved_at' => null,
+        ]);
+    }
+
+    /** @test */
+    public function user_can_resolve_report_using_id()
+    {
+        $this->login();
+        $station = Station::factory()->create();
+        /** @var Collection */
+        $resolved = Report::factory(2)->create([
+            'station_id' => $station->id
+        ]);
+        $unresolved = Report::factory(3)->create([
+            'station_id' => $station->id
+        ]);
+
+        $response = $this->putJson(
+            route('station.report.resolve', $station),
+            [
+                'report_id' => $resolved->pluck('id'),
+            ]
+        );
+
+        $response->assertOk();
+        $this->assertEquals(2, Report::whereNotNull('resolved_at')->count());
+        $this->assertEquals(3, Report::whereNull('resolved_at')->count());
     }
 }
