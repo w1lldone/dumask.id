77f58f9bb15417c8438aae5ab99f62fdb1b20c44
w1lldone <wildan.arrahman@gmail.com>
2021-06-03 13:15:06 +0700

    Fix title

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
+
+@section('title', 'Laporkan kondisi station')
+
 @section('content')
 
 <div class=" d-flex-column my-4 mx-auto w-md-50">
     <div class="flex card border-0 px-4 py-4" style="border-radius:0.75em">
-        <report-create-form 
+        <report-create-form
             :station='{{ json_encode($station) }}'
             :conditions='@json(\App\Models\Report::$conditions)'
         ></report-create-form>
