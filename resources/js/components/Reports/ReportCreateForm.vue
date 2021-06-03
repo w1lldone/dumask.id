8a336467d24b3de59118add9572c5b07a110bfb6
Michael Roberto <46708593+michaelr1300@users.noreply.github.com>
2021-06-03 14:28:23 +0700

    Report UI (#126)
    * Report UI
    
    * Fix component (#127)
    
    * Fix component
    
    * Add page title
    
    * Fix title
    
    Co-authored-by: Wildan Ainurrahman <w1lldone@users.noreply.github.com>

--- resources/js/components/Reports/ReportCreateForm.vue
+++ resources/js/components/Reports/ReportCreateForm.vue
@@ -1,118 +1,128 @@
 <template>
-  <div>
+  <form @submit.prevent="doSubmit()">
     <div class="d-flex">
-      <h5 class="text-dark font-weight-bold my-auto">
-          REPORT STATION
+      <h5 class="text-secondary font-weight-bold my-auto">
+          Laporkan Kondisi Station
       </h5>
     </div>
     <hr style="border-bottom: 2px solid #c4c4c4;">
+
     <div class="text-left">
-
-      <div class="form-group">
-        <label class="font-weight-bold" for="condition">
-          Kondisi Dropbox
-          <abbr class="text-danger">*</abbr>
-        </label>
-        <div class="custom-control custom-radio"
-          v-for="(condition, index) in conditions"
-          :key="index"
-        >
-            <input
-              :name="'conditon'"
-              :value="index"
-              type="radio"
-              class="custom-control-input"
-              :id="'condition-' + index"
-              v-model="form.condition"
-            />
-            <label class="custom-control-label" :for="'condition-' + index">
-              {{ condition }}
-            </label>
-        </div>
-        <div 
-          class="invalid-feedback"
-          :class="{ 'd-block': hasErrors('condition') }"
-        >
-          {{ getErrors("condition") }}
-        </div>
-      </div>
-
-      <div>
-        
-      </div>
-
-      <div class="form-group">
-        <label class="font-weight-bold" for="location-checkbox">Kirimkan Lokasi</label>
-        <div class="d-flex">
-          <div class="custom-control custom-checkbox mr-4">
-            <input
-              type="checkbox"
-              class="custom-control-input"
-              id="location-checkbox"
-              v-model="form.send_location"
-            />
-            <label
-              class="custom-control-label"
-              for="location-checkbox"
-              >
-              Kirimkan lokasi saya saat ini (Pastikan GPS anda Aktif) <br>
-              Menyertakan lokasi dan foto bisa membantu pengelola dropbox memproses laporan Anda lebih cepat
-            </label
-            >
+      <h4 class="text-secondary font-weight-bold" style="text-transform: uppercase;">{{ station.name }}</h4>
+      <span class="mdi mdi-archive-outline text-secondary"></span>
+      <span class="text-secondary font-weight-bold align-middle ml-2">{{ station.dropboxes_count }} Dropbox</span>
+      <div class="mt-4">
+        <div class="form-group">
+          <label class="font-weight-bold" for="condition">
+            Bagaimana kondisi dropbox di station ini?
+            <small class="text-muted">(wajib diisi)</small>
+          </label>
+          <div class="custom-control custom-radio"
+            v-for="(condition, index) in conditions"
+            :key="index"
+          >
+              <input
+                required
+                name="conditon"
+                :value="index"
+                type="radio"
+                class="custom-control-input"
+                :id="'condition-' + index"
+                v-model="form.condition"
+              />
+              <label class="custom-control-label" :for="'condition-' + index">
+                {{ condition }}
+              </label>
+          </div>
+          <div
+            class="invalid-feedback"
+            :class="{ 'd-block': hasErrors('condition') }"
+          >
+            {{ getErrors("condition") }}
           </div>
         </div>
-      </div>
 
-      <div class="form-group">
-        <label class="font-weight-bold" for="photo">
-          Foto (Opsional)
-        </label>
-        <div class="col-md-3 px-0">
-          <label
-            class="btn btn-outline-primary d-flex w-md-50"
-          >
-            <div class="mdi mdi-image"></div>
-            <div class="my-auto">Upload Image</div>
-              <div id="uploadPhotoForm">
-                <input
-                  id="inputPhoto"
-                  type="file"
-                  style="display: none"
-                  ref="photo"
-                  name="photo"
-                  accept="image/*"
-                  @change="getNewFileName"
-                />
-              </div>
+        <div class="form-group">
+          <label class="font-weight-bold" for="photo">
+            Unggah foto pendukung
+            <small class="text-muted">(opsional)</small>
           </label>
+          <div class="col-md-3 px-0">
+            <label
+              class="btn btn-outline-secondary rounded d-flex"
+            >
+              <div class="mdi mdi-image"></div>
+              <div class="my-auto">Unggah foto</div>
+                <div id="uploadPhotoForm">
+                  <input
+                    id="inputPhoto"
+                    type="file"
+                    style="display: none"
+                    ref="photo"
+                    name="photo"
+                    accept="image/*"
+                    @change="getNewFileName"
+                  />
+                </div>
+            </label>
+          </div>
+          <div
+            id='photoName'
+          >
+            {{fileName}}
+          </div>
+          <small class="text-muted">Foto yang anda kirimkan dapat membantu kami memproses laporan dengan lebih tepat.</small>
         </div>
-        <div
-          id='photoName'
-        >
-          {{fileName}}
+
+        <div class="form-group">
+
+
+          <div>
+            <div class="custom-control custom-checkbox mr-4">
+              <input
+                type="checkbox"
+                class="custom-control-input"
+                id="location-checkbox"
+                v-model="form.send_location"
+              />
+              <label
+                class="custom-control-label font-weight-bold"
+                for="location-checkbox"
+                >
+                Saya setuju mengirimkan lokasi saya saat ini
+                <small class="text-muted">(opsional)</small>
+              </label>
+              <div>
+              </div>
+            </div>
+            <small class="text-muted">Lokasi yang Anda kirimkan dapat membantu kami memproses laporan dengan lebih cepat.</small>
+          </div>
         </div>
-      </div>
-      <div class="form-group mt-4 text-right">
-        <button
-          style="background: #A7A7A7"
-          class="btn text-white shadow mx-2"
-          @click="doReset()"
-        >
-          RESET
-        </button>
-        <button
-          class="btn btn-primary shadow"
-          v-show="!isLoading"
-          @click="doSubmit()"
-        >
-          SUBMIT
-        </button>
-        <button class="btn btn-primary" disabled v-show="isLoading">
-          SUBMITTING...
-        </button>
+
+
+        <div class="form-group mt-4 text-right">
+          <button
+            type="button"
+            style="background: #A7A7A7"
+            class="btn text-white shadow mx-2"
+            @click="doReset()"
+          >
+            RESET
+          </button>
+          <button
+            type="submit"
+            class="btn btn-primary shadow"
+            v-show="!isLoading"
+          >
+            SUBMIT
+          </button>
+          <button class="btn btn-primary" disabled v-show="isLoading">
+            SUBMITTING...
+          </button>
+        </div>
       </div>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
@@ -126,7 +136,7 @@ export default {
     conditions: {
       type: Object
     }
-    
+
   },
 
   data() {
@@ -148,7 +158,6 @@ export default {
   methods: {
     async doSubmit() {
       this.isLoading = true;
-
       var url = "../station/" + this.station.id + "/report";
 
       var config = {
@@ -160,15 +169,20 @@ export default {
       var formData = new FormData();
 
       if (this.isPhotoIncluded) {
-        var photo = this.$refs.photo.files[0];  
+        var photo = this.$refs.photo.files[0];
         formData.append('photo', photo);
       }
 
       if (this.form.send_location) {
-        this.form.user_latitude = null,
-        this.form.user_longitude= null,
-        await this.getUserLocation();
-      } 
+        this.form.user_latitude = null
+        this.form.user_longitude= null
+        try {
+            await this.getUserLocation();
+        } catch (error) {
+            this.isLoading = false
+            return alert("Kami tidak dapat mendapatkan lokasi Anda. Pastikan GPS Anda aktif lalu silahkan coba lagi.");
+        }
+      }
       else {
         this.form.user_latitude = '',
         this.form.user_longitude= '';
@@ -180,12 +194,13 @@ export default {
 
       try {
         let response = await axios.post(url, formData, config);
-        alert("Report submitted!");
-        return location.reload();
+        alert("Terima kasih atas laporan Anda!");
+        return location.href = "/";
       } catch (error) {
-        alert(error.response.data.message);
-        console.log(error.response);
-        this.errors = error.response.data.errors;
+        alert(error.response.data.message ?? error.response.data);
+        if (error.response.status == 422) {
+            this.errors = error.response.data.errors;
+        }
       }
 
       this.isLoading = false;
@@ -223,14 +238,9 @@ export default {
     },
 
     async getUserLocation() {
-      try {
         const position = await this.getCoordinates();
         this.form.user_latitude = position.coords.latitude;
         this.form.user_longitude = position.coords.longitude;
-      } catch (error) {
-        alert("Please turn on your location service and try again.");
-        this.isLoading = false;
-      }
     },
 
     getNewFileName(event){
--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -1,19 +1,12 @@
-@extends('layouts.template')
+@extends('layouts.app')
+
+@section('title', 'Laporkan kondisi station')
+
 @section('content')
 
-<div class="row">
-    <div class="col-12 flex px-md-0 pt-sm-0 w-100">
-        <div class="col-12 col-md-4 flex card border-0 mt-4 py-2" style="border-radius:0.75em">
-            <h2 class="text-secondary font-weight-bold">REPORT</h2>
-            <span>
-                
-            </span>
-        </div>
-    </div>
-</div>
-<div class="mt-4 d-flex-column">
+<div class=" d-flex-column my-4 mx-auto w-md-50">
     <div class="flex card border-0 px-4 py-4" style="border-radius:0.75em">
-        <report-create-form 
+        <report-create-form
             :station='{{ json_encode($station) }}'
             :conditions='@json(\App\Models\Report::$conditions)'
         ></report-create-form>
