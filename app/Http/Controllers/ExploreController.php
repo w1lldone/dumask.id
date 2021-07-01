5162930e6557f1ab134719818d81e09bb6c279d2
w1lldone <wildan.arrahman@gmail.com>
2021-07-01 14:57:57 +0700

    Handle error on empty stations

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -18,7 +18,7 @@ class ExploreController extends Controller
     {
         $stats['total_weight'] = round(DropboxLog::getTotalWeight()/1000,1);
         $stats['total_stations'] = Station::count();
-        $stats['last_operation_at'] = Station::orderByDesc('last_operation_at')->first()->last_operation_at->format('d F Y');
+        $stats['last_operation_at'] = Station::orderByDesc('last_operation_at')->first() ? Station::orderByDesc('last_operation_at')->first()->last_operation_at->format('d F Y') : '';
 
         return view('explore.explore', compact('stats'));
     }
